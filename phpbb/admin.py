4877e1592111e92b1aff1ffb98cc520ebe2f1d7c
maciej.blizinski <maciej.blizinski@2cc4e26e-97ec-11dd-87e9-a30ea5446288>
2008-11-27 21:48:48 +0000

    Adding admin interface file.
    git-svn-id: http://django-phpbb.googlecode.com/svn/trunk@13 2cc4e26e-97ec-11dd-87e9-a30ea5446288

---
+++ phpbb/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.phpbb.models import ForumForum
+from django.contrib.phpbb.models import ForumPost
+from django.contrib.phpbb.models import ForumTopic
+from django.contrib.phpbb.models import ForumUser
+
+class ForumForumAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(ForumForum, ForumForumAdmin)
+class ForumTopicAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(ForumTopic, ForumTopicAdmin)
+class ForumPostAdmin(admin.ModelAdmin):
+    list_display = ('post_id', 'get_absolute_url', 'get_time', )
+admin.site.register(ForumPost, ForumPostAdmin)
+class ForumUserAdmin(admin.ModelAdmin):
+    list_display = ('username',
+                    'user_id',
+                    'user_regdate',
+                    'user_posts',
+                    'user_email', )
+admin.site.register(ForumUser, ForumUserAdmin)
